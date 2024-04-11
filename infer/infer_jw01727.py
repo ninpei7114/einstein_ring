956b8f2bd82de83f9858f28d456035315926b828
ninpei7114 <ninpei7114@gmail.com>
2024-04-11 19:54:45 +0900

    tarfile_dirのargsを削除

--- infer/infer_jw01727.py
+++ infer/infer_jw01727.py
@@ -61,13 +61,13 @@ def main(args):
         region_name = region.split("/")[-1].split(".")[0]
         print(f"\n{region_name=}")
 
-        os.makedirs(f"{args.tarfile_dir}/{region_name}", exist_ok=True)
-        shutil.copy(f"{args.data_dir}/png_file/*{region_name}.png", f"{args.tarfile_dir}/{region_name}")
+        os.makedirs(f"{tarfile_dir}/{region_name}", exist_ok=True)
+        shutil.copy(f"{args.data_dir}/png_file/*{region_name}.png", f"{tarfile_dir}/{region_name}")
 
         tarfile_name = f"{tarfile_dir}/{region_name}_dataset.tar"
         if not os.path.exists(tarfile_name):
             with tarfile.open(tarfile_name, "w:gz") as tar:
-                tar.add(f"{args.tarfile_dir}/{region_name}")
+                tar.add(f"{tarfile_dir}/{region_name}")
 
         Dataset_test = (
             webdataset.WebDataset(tarfile_name).decode("pil").to_tuple("png", "__key__").map(preprocess_validation)
